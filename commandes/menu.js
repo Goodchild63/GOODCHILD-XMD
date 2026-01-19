ccaf4a4808e104d203275960636e5992d2c0a672
Goodchild63 <willygoodchild@gmail.com>
2026-01-19 15:03:25 +0300

    Fix formatting in menu display for commands

--- commandes/menu.js
+++ commandes/menu.js
@@ -46,7 +46,7 @@ const date = moment().format('DD/MM/YYYY');
 ┌┤ `;
         for (const cmd of coms[cat]) {
             menuMsg += `          
-┃│𒊹  ${cmd}`    
+┃│>  ${cmd}`    
         } 
         menuMsg +=`
 ┌┤
